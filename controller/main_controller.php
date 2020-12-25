bc4bf4aaa5d26d188a6bb05ee984573a734e8521
Rob Gregory <2349765+MintyMods@users.noreply.github.com>
2020-12-25 16:15:36 +0000

    resolved issue with dispaying BBCODE

--- controller/main_controller.php
+++ controller/main_controller.php
@@ -101,14 +101,20 @@ class main_controller {
 
 
 	public function selectPostForSection($post_id, $section, $topic_id, $forum_id) {
+		
 		global $db, $auth, $user;
+		
 		if (!class_exists('bbcode')) {
-		   include($this->phpbb_root_path . 'includes/bbcode' . $this->php_ext);
+			include_once($this->phpbb_root_path . 'includes/bbcode' . $this->php_ext);
 		}
 
+		// if (!function_exists('display_custom_bbcodes'))	{
+			//include_once($this->root_path . 'includes/functions_display' . $this->php_ext);
+		// }
+		
 		$posts_ary = array(
 			'SELECT'    => 'p.*',
-				'FROM'      => array(
+			'FROM'      => array(
 				POSTS_TABLE     => 'p',
 				$this->table_name => 'homepage',
 			),
@@ -133,10 +139,7 @@ class main_controller {
 			$post_author = get_username_string('full', $posts_row['poster_id'], $posts_row['username'], $posts_row['user_colour']);
 			$post_date = $user->format_date($posts_row['post_time']);
 			$post_link = append_sid($this->phpbb_root_path . "viewtopic" . $this->php_ext, 'f=' . $posts_row['forum_id'] . '&amp;t=' . $posts_row['topic_id'] . '&amp;p=' . $posts_row['post_id']) . '#p' . $posts_row['post_id'];
-			$post_text = nl2br($posts_row['post_text']);
-			$bbcode = new \bbcode(base64_encode($bbcode_bitfield));         
-			$bbcode->bbcode_second_pass($post_text, $posts_row['bbcode_uid'], $posts_row['bbcode_bitfield']);
-			$post_text = smiley_text($post_text);
+			$post_text = generate_text_for_display( $posts_row[ 'post_text' ], $posts_row[ 'bbcode_uid' ], $posts_row[ 'bbcode_bitfield' ], 7 );
 	
 			$this->template->assign_block_vars('HOMEPAGE_POSTS', array(
 			 'TOPIC_TITLE'       => censor_text($topic_title),
@@ -146,59 +149,6 @@ class main_controller {
 			 'POST_TEXT'         => censor_text($post_text),
 			));
 		}
-	}
-
-	public function selectPost($post_id, $section, $topic_id, $forum_id) {
-		global $db, $auth, $user;
-
-		$search_limit = 5;
-		$posts_ary = array(
-			'SELECT'    => 'p.*, t.*, u.username, u.user_colour',
-				'FROM'      => array(
-				POSTS_TABLE     => 'p',
-			),
-				'LEFT_JOIN' => array(
-				array(
-					'FROM'  => array(USERS_TABLE => 'u'),
-					'ON'    => 'u.user_id = p.poster_id'
-				),
-				array(
-					'FROM'  => array(TOPICS_TABLE => 't'),
-					'ON'    => 'p.topic_id = t.topic_id'
-				),
-			),
-			'WHERE'     =>  str_replace( array('WHERE ', 'topic_id'), array('', 't.topic_id'), $topic_id) . '
-					AND t.topic_status <> ' . ITEM_MOVED . '
-					AND t.topic_visibility = 1',
-			'ORDER_BY'  => 'p.post_id DESC',
-		);
-		
-		$posts = $db->sql_build_query('SELECT', $posts_ary);
-	    $posts_result =$db->sql_query_limit($posts, $search_limit);
-	
-		  while( $posts_row = $db->sql_fetchrow($posts_result) ) {
-			 $topic_title       = $posts_row['topic_title'];
-			 $post_author       = get_username_string('full', $posts_row['poster_id'], $posts_row['username'], $posts_row['user_colour']);
-			 $post_date          = $user->format_date($posts_row['post_time']);
-			 $post_link       = append_sid("{$phpbb_root_path}viewtopic.$phpEx", 'f=' . $posts_row['forum_id'] . '&amp;t=' . $posts_row['topic_id'] . '&amp;p=' . $posts_row['post_id']) . '#p' . $posts_row['post_id'];
-			 $post_text = nl2br($posts_row['post_text']);
-			
-			 if (!class_exists('bbcode')) {
-				include($this->phpbb_root_path . 'includes/bbcode' . $this->php_ext);
- 			 }
-
-			 $bbcode = new \bbcode(base64_encode($bbcode_bitfield));         
-			 $bbcode->bbcode_second_pass($post_text, $posts_row['bbcode_uid'], $posts_row['bbcode_bitfield']);
-			 $post_text = smiley_text($post_text);
-	
-			 $this->template->assign_block_vars('HOMEPAGE_POSTS', array(
-			 'TOPIC_TITLE'       => censor_text($topic_title),
-			 'POST_AUTHOR'       => $post_author,
-			 'POST_DATE'       	 => $post_date,
-			 'POST_LINK'      	 => $post_link,
-			 'POST_TEXT'         => censor_text($post_text),
-			 ));
-		  }
 	}
 
 }
